81162cd502683c6685bec5874435e6d350c70af4
SesamPlayz <43842481+SesamPlayz@users.noreply.github.com>
2019-10-03 15:27:42 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+$geil = "Geilheitsgrad:";
+$grad = 99;
+echo "$geil$grad <br>";
+echo "Hallo Welt<br>";
+echo "Moien";
+echo "Moien";
+echo "<p style=\"text-align: center;\">Moin</p><br>";
+$vor = "Sebastian";
+$nach = "Melone";
+echo "<p style=\"text-align: center;\">Herzlich Willkommen $vor $nach<br>zum[\"PHP-Kurs.com\"]</p>";
+echo $_SERVER["REMOTE_ADDR"], "<br>";
+echo $_SERVER["HTTP_HOST"], "<br>"; 
+?>
